6c97db7389714bf3020d7de1f30c84e6fae528c9
Karla Buckner, Nate McGregor and Senya Webster <senya@>
2017-10-24 11:17:41 -0700

    now allow user to edit tasks and choose priority in UI

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,16 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-     <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-     </ul>
-   </div>
+  <div class="container">
+        <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+        <h3>{{currentFocus}}</h3>
+        <ul>
+          <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+        </ul>
+        <hr>
+        <div>
+          <div *ngIf="selectedTask">
+            <h3>{{selectedTask.description}}</h3>
+            <p>Task Complete? {{selectedTask.done}}</p>
+            <hr>
+            <h3>Edit Task</h3>
+            <label>Enter Task Description:</label>
+            <input [(ngModel)]="selectedTask.description">
+            <label>Enter Task Priority (1-3):</label><br>
+            <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+            <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+            <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+            <button (click)="finishedEditing()">Done</button>
+          </div>
+        </div>
+      </div>
   `
 })
-
 
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
@@ -21,13 +36,40 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
-  ];
+   new Task('Finish weekend Angular homework for Epicodus course', 3),
+   new Task('Begin brainstorming possible JavaScript group projects', 2),
+   new Task('Add README file to last few Angular repos on GitHub', 1)];
+      selectedTask = null;
+
+isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!");
+    }
+  }
+
+  priorityColor(currentTask){
+     if (currentTask.priority === 3){
+       return "bg-danger";
+     } else if (currentTask.priority === 2) {
+       return  "bg-warning";
+     } else {
+       return "bg-info";
+     }
+   }
+
+   editTask(clickedTask) {
+     this.selectedTask = clickedTask;
+   }
+
+   finishedEditing() {
+   this.selectedTask = null;
+ }
+
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public description: string, public priority: number) {   }
 }
